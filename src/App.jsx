c20a326a8aefff9e4f3b978fda4de963dfc5ea48
abufahimkhan <abufahimkhan1@gmail.com>
2024-04-18 13:40:15 +0600

    2nd commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import PlaceCard from './components/PlaceCards/PlaceCard';
+import ServiceCard from './components/ServiceCards/ServiceCard';
+import Layout from "./pages/Layout";
+import Footer from './components/FooterSection/Footer';
+import SignupForm from './components/Registration/SignupForm';
+import LoginForm from './components/Registration/LoginForm';
+import Booking from './components/PlaceCards/Booking';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Layout><Home /></Layout>} />
+        <Route path="/travels" element={<Layout><PlaceCard /></Layout>} />
+        <Route path="/services" element={<Layout><ServiceCard /></Layout>} />
+        <Route path="/about" element={<Layout><Home /></Layout>} />
+        <Route path="/contact" element={<Footer />} />
+        <Route path="/booking" element={<Layout><Booking /></Layout>} />
+        {/* Login and Signup routes outside the layout */}
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+      </Routes>
+    </Router>
+  );
+}
